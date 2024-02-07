5477e035bef9bd120703a70c31278aa216f20bdc
Koul <dongik0122@gmail.com>
2024-02-07 23:53:55 +0900

    #4 - start_spring
    - DI, IOC, 스프링 컨테이너에 대한 개념을 학습하였다
    
    - 기존의 AppConfig는 순수 자바를 이용하여 구현하였지만 스프링을 이용하는 것으로 바꿨다.
    
    - @Configuration, @Bean의 작동 원리에 대해 파악화였다.

--- src/main/java/tomist/core/AppConfigByJAVA.java
+++ src/main/java/tomist/core/AppConfigByJAVA.java
@@ -1,9 +1,6 @@
 package tomist.core;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import tomist.core.discount.DiscountPolicy;
-import tomist.core.discount.FixDiscountPolicy;
 import tomist.core.discount.RateDiscountPolicy;
 import tomist.core.member.MemberRepository;
 import tomist.core.member.MemberService;
@@ -15,26 +12,20 @@ import tomist.core.order.OrderServiceImpl;
 //역할을 드러나게 하는 것이 중요하다.
 //메서드 명으로 역할이 드러난다.
 //중복 제거가 중요하다.
-@Configuration
-public class AppConfig {
-
+public class AppConfigByJAVA {
   //생성자 주입
-  @Bean //스프링 컨테이너에 등록 된다.
   public MemberService memberService(){
     return new MemberServiceImpl(memberRepository());
   }
 
-  @Bean
-  public static MemberRepository memberRepository() {
+  private static MemberRepository memberRepository() {
     return new MemoryMemberRepository();
   }
 
-  @Bean
   public OrderService orderService(){
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
-  @Bean
   public DiscountPolicy discountPolicy(){
 //    return new FixDiscountPolicy();
     return new RateDiscountPolicy();
--- src/main/java/tomist/core/MemberApp.java
+++ src/main/java/tomist/core/MemberApp.java
@@ -1,5 +1,7 @@
 package tomist.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import tomist.core.member.Grade;
 import tomist.core.member.Member;
 import tomist.core.member.MemberService;
@@ -7,9 +9,12 @@ import tomist.core.member.MemberService;
 //TEST 목적으로 사용
 public class MemberApp {
   public static void main(String[] args) {
-    AppConfig appConfig = new AppConfig();
-    MemberService memberService = appConfig.memberService();
+//    AppConfig appConfig = new AppConfig();
+//    MemberService memberService = appConfig.memberService();
 //    MemberService memberService = new MemberServiceImpl();
+
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
     Member member = new Member(1L, "dongik", Grade.VIP);
     memberService.join(member);
