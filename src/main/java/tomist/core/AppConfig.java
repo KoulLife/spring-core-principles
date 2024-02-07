7a4e8b4f2991de7c316750247d9a1e0719cbbb2d
Koul <dongik0122@gmail.com>
2024-02-07 23:25:09 +0900

    #3 - SOLID
    객체지향설계의 원칙을 지켜 AppConfig를 통해 관심사 분리를 했음

---
+++ src/main/java/tomist/core/AppConfig.java
@@ -0,0 +1,34 @@
+package tomist.core;
+
+import tomist.core.discount.DiscountPolicy;
+import tomist.core.discount.FixDiscountPolicy;
+import tomist.core.discount.RateDiscountPolicy;
+import tomist.core.member.MemberRepository;
+import tomist.core.member.MemberService;
+import tomist.core.member.MemberServiceImpl;
+import tomist.core.member.MemoryMemberRepository;
+import tomist.core.order.OrderService;
+import tomist.core.order.OrderServiceImpl;
+
+//역할을 드러나게 하는 것이 중요하다.
+//메서드 명으로 역할이 드러난다.
+//중복 제거가 중요하다.
+public class AppConfig {
+  //생성자 주입
+  public MemberService memberService(){
+    return new MemberServiceImpl(memberRepository());
+  }
+
+  private static MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+
+  public OrderService orderService(){
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+  }
+
+  public DiscountPolicy discountPolicy(){
+//    return new FixDiscountPolicy();
+    return new RateDiscountPolicy();
+  }
+}
