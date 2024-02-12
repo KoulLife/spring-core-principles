cbd35c29520e583d8c1d3437c907e54e0b697310
Koul <dongik0122@gmail.com>
2024-02-13 00:17:47 +0900

    싱글톤 패턴 만들어보기
    싱글톤 패턴을 만들어보는 실습을 하였다.

--- src/test/java/tomist/core/singletone/SingletonTest.java
+++ src/test/java/tomist/core/singletone/SingletonTest.java
@@ -1,5 +1,6 @@
 package tomist.core.singletone;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import tomist.core.AppConfig;
@@ -20,4 +21,11 @@ public class SingletonTest {
         System.out.println("memberService1 = " + memberService2);
     }
 
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+        Assertions.assertThat(singletonService1).isEqualTo(singletonService2);
+    }
 }
